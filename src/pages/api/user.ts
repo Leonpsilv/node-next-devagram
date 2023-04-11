547dbdb904a248c338fde8dea2363208d3608b27
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-04-11 09:48:43 -0400

    Update user.ts

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,12 +1,24 @@
 import { NextApiRequest,NextApiResponse } from "next";
 import { tokenJWTValidator } from "@/middlewares/tokenJWTValidator";
+import { dbConnect } from "@/middlewares/dbConnect";
+import { UserModel } from "@/models/userModel";
+import { defaultResponsesMsg } from "@/types/defaultResponsesMsg";
 
 const userEndPoint = async (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<defaultResponsesMsg| any>
 ) => {
-    return res.status(200).json({msg: 'usuario autenticado com sucesso'})
+    try{
+        const {userId} = req?.query
+        const user = await UserModel.findOne({_id: userId})
+        if(!user) return res.status(404).json({error: 'usuário informado não encontrado'})
+        user.password = undefined
+    
+        return res.status(200).json(user)
+    }catch(e){
+        return res.status(400).json({error: 'não foi possível obter os dados do usuário'})
+    }
 }
 
 
-export default tokenJWTValidator(userEndPoint)
+export default tokenJWTValidator(dbConnect( userEndPoint))
