3e9c184978e808f57e24f5f3c29836b37e4b3554
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-04-12 10:53:04 -0400

    feed home

--- src/pages/api/feed.ts
+++ src/pages/api/feed.ts
@@ -4,30 +4,62 @@ import { dbConnect } from "@/middlewares/dbConnect";
 import { UserModel } from "@/models/userModel";
 import { defaultResponsesMsg } from "@/types/defaultResponsesMsg";
 import { PostModel } from "@/models/postModel";
+import { FollowModel } from "@/models/followModel";
 
-const userEndPoint = async (
+const feedEndPoint = async (
     req: NextApiRequest,
     res: NextApiResponse<defaultResponsesMsg| any>
 ) => {
     try{
         if(req.method === 'GET'){
-            if(!req?.query.id) return res.status(400).json({error: 'usuário não informado'})
-            const anUserId = req?.query.id
-            const user = await UserModel.findById(anUserId)
-            if(!user) return res.status(404).json({error: 'usuário informado não encontrado'})
-            user.password = undefined
-        
-            const posts = await PostModel
-                .find({userId: anUserId})
+            if(req?.query.id){
+                const anUserId = req?.query.id
+                const user = await UserModel.findById(anUserId)
+                if(!user) return res.status(400).json({error: 'usuário informado não encontrado'})
+                user.password = undefined
+            
+                const posts = await PostModel
+                    .find({userId: anUserId})
+                    .sort({date: -1})
+            
+                return res.status(200).json(posts)
+            }else {
+                const {userId} = req?.query
+                const user = await UserModel.findById(userId)
+                if(!user) return res.status(400).json({error: 'usuário não encontrado'})
+
+                const following = await FollowModel.find({userId})
+                const followingIds = following.map(user => user.userIdFollowed)
+
+                const posts = await PostModel.find({
+                    $or:[
+                        {userId},
+                        {userId: followingIds}
+                    ]
+                })
                 .sort({date: -1})
-        
-            return res.status(200).json(posts)
+
+                const result = []
+                for (const post of posts) {
+                    const postUser = await UserModel.findById(post.userId)
+                    if(postUser){
+                        const final = {...post._doc, user: {
+                            name: postUser.name,
+                            avatar: postUser.avatar
+                        }}
+                        result.push(final)
+                    }
+                }
+
+                return res.status(200).json(result)
+            }
         }
         return res.status(405).json({error: 'método informado não é válido'})
     }catch(e){
+        // console.log(e)
         return res.status(400).json({error: 'não foi possível obter os dados desejados'})
     }
 }
 
 
-export default tokenJWTValidator(dbConnect( userEndPoint))
+export default tokenJWTValidator(dbConnect(feedEndPoint))
