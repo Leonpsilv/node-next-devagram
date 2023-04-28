840ad9cae1b3d6bcd21d19eb96e3f93f95c877af
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-04-27 20:27:06 -0400

    following info

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -4,6 +4,7 @@ import { dbConnect } from "@/middlewares/dbConnect";
 import { UserModel } from "@/models/userModel";
 import { defaultResponsesMsg } from "@/types/defaultResponsesMsg";
 import { corsPolicy } from "@/middlewares/corsPolicy";
+import { FollowModel } from "@/models/followModel";
 
 const searchEndPoit = async (
     req: NextApiRequest,
@@ -18,7 +19,21 @@ const searchEndPoit = async (
                 if(!searchUser) return res.status(400).json({error: 'usuário não encontrado'})
                 searchUser.password = undefined
 
-                return res.status(200).json(searchUser)
+                const follow = await FollowModel.find({userId, userIdFollowed: req?.query?.id})
+                const followBoolean = follow.length === 0 ? false : true
+                const formatedData = {
+                    _id: searchUser._id,
+                    name: searchUser.name,
+                    email: searchUser.email,
+                    avatar: searchUser.avatar,
+                    followers: searchUser.followers,
+                    following: searchUser.following,
+                    posts: searchUser.posts,
+                    __v: searchUser.__v,
+                    followingUser: followBoolean
+                }
+
+                return res.status(200).json(formatedData)
             }else{
                 const {filter} = req.query
                 if(!filter || filter.length < 2) return res.status(204).json(null)
